4ee0244231d8b24880c6e72af5f88007db38a3c9
Laurent Thomas <laurent132.thomas@laposte.net>
2020-08-11 10:18:01 +0200

    gold standard docstring formatting

--- IM/Metadata.py
+++ IM/Metadata.py
@@ -1,9 +1,11 @@
-'''
+"""
+Extract IM metadata from filename.
+
 Python 2 (incl.Fiji) or 3
 This class contains a set of function to extract metadata by parsing the image file names string of images acquired on an IM04
 example filename : "-A001--PO01--LO001--CO6--SL001--PX32500--PW0080--IN0020--TM244--X014580--Y011262--Z209501--T1374031802--WE00001.tif"
 It also contains a function convertXY_PixToIM to convert from pixel coordinates in an image to the corresponding machine coordinates
-'''
+"""
 from __future__ import division
 import string
 
@@ -24,7 +26,7 @@ magToNA = {2:0.06,
 		   40:0.6}
 
 def getXYPosition_mm(imageName):
-	'''Extract the XY-axis coordinates (in mm) from the imageName (for IM4). The coordinates corresponds to the center of the image = objective position.'''
+	"""Extract the XY-axis coordinates (in mm) from the imageName (for IM4). The coordinates corresponds to the center of the image = objective position."""
 	# Parse string + do conversion
 	X0mm = int(imageName[65:71]) /1000 # >0
 	Y0mm = int(imageName[74:80]) /1000
@@ -32,19 +34,19 @@ def getXYPosition_mm(imageName):
 	return X0mm, Y0mm
 
 def getWellSubPosition(imageName):
-	'''Extract the index corresponding to the subposition for that well'''
+	"""Extract the index corresponding to the subposition for that well."""
 	return int(imageName[9:11])
 
 def getZPosition_um(imageName):
-	'''Extract the Z-axis coordinates (in um)'''
+	"""Extract the Z-axis coordinates (in um)."""
 	return float(imageName[83:89])/10
 	
 def getPixelSize_um(imageName):
-	'''Extract the pixel size (in um) from the imageName (for IM4)'''
+	"""Extract the pixel size (in um) from the imageName (for IM4)."""
 	return float(imageName[34:39])*10**-4
 	
 def getObjectiveMagnification(imageName):
-	'''Get the magnification as integer'''
+	"""Get the magnification as integer."""
 	pixSize = getPixelSize_um(imageName)
 	if pixelSizeToMag.has_key(pixSize):
 		return pixelSizeToMag[pixSize]
@@ -52,7 +54,7 @@ def getObjectiveMagnification(imageName):
 		raise KeyError("No pixel size matching in the pixelSizeToMag dictionnary")
 
 def getObjectiveNA(imageName):
-	'''Return the Numerical Aperture of the objective'''
+	"""Return the Numerical Aperture of the objective."""
 	pixSize = getPixelSize_um(imageName)
 	if pixelSizeToNA.has_key(pixSize):
 		return pixelSizeToNA[pixSize]
@@ -60,60 +62,62 @@ def getObjectiveNA(imageName):
 		raise KeyError("No pixel size matching in the pixelSizeToNA dictionnary")
 	
 def getWellId(imageName):
-	'''Extract well Id (ex:A001) from the imageName (for IM4)'''
+	"""Extract well Id (ex:A001) from the imageName (for IM4)."""
 	return imageName[1:5]
 
 def getWellColumn(imageName):
-	'''Extract well column (1-12) from the imageName (for IM4)'''
+	"""Extract well column (1-12) from the imageName (for IM4)."""
 	return int(imageName[2:5])
 
 def getWellRow(imageName):
-	'''Extract well row (1-8) from the imageName (for IM4)'''
+	"""Extract well row (1-8) from the imageName (for IM4)."""
 	letter = imageName[1:2]
 	return string.uppercase.index(letter)+1 # alphabetical order +1 since starts at 0
 
 
 def getWellIndex(imageName):
-	'''Return well number corresponding to order of acquisition by the IM (snake pattern)'''
+	"""Return well number corresponding to order of acquisition by the IM (snake pattern)."""
 	return int(imageName[106:111]) 
 
 	
 def getZSlice(imageName):
-	'''Return image slice number of the associated Z-Stack serie'''
+	"""Return image slice number of the associated Z-Stack serie."""
 	return int(imageName[27:30])
 
 	
 def getChannelIndex(imageName):
-	'''Return integer index of the image channel
-	1 = DAPI (385)
-	3 = FITC (GFP...)
-	5 = TRITC (mCherry...)
-	'''
-	return int(imageName[22:23])
+    """
+    Return integer index of the image channel.
+    
+    1 = DAPI (385)
+    3 = FITC (GFP...)
+    5 = TRITC (mCherry...)
+    """
+    return int(imageName[22:23])
 
 	
 def getLoopIteration(imageName):
-	'''Return the integer index corresponding to the image timepoint'''
+	"""Return the integer index corresponding to the image timepoint."""
 	return int(imageName[15:18])
 
 	
 def getLightPower(imageName):
-	'''Return relative power (%) used for the acquisition with this channel'''
+	"""Return relative power (%) used for the acquisition with this channel."""
 	return int(imageName[43:47])
 
 	
 def getLightExposure(imageName):
-	'''Return exposure time in ms used for the acquisition with this channel'''
+	"""Return exposure time in ms used for the acquisition with this channel."""
 	return int(imageName[51:55])
 
 def getTemperature(imageName):
-	'''Return temperature in celsius degrees as measured by the probe at time of acquisition'''
+	"""Return temperature in celsius degrees as measured by the probe at time of acquisition."""
 	return float(imageName[59:62])/10
 
 
 def convertXY_PixToIM(Xpix, Ypix, PixelSize_um, X0mm, Y0mm, Image_Width=2048, Image_Height=2048):
-	'''
-	Convert XY pixel coordinates of some item in an image acquired with the Acquifer IM, to the corresponding XY IM coordinates
+	"""
+	Convert XY pixel coordinates of some item in an image acquired with the Acquifer IM, to the corresponding XY IM coordinates.
 	
 	Parameters
 	----------
@@ -127,10 +131,10 @@ def convertXY_PixToIM(Xpix, Ypix, PixelSize_um, X0mm, Y0mm, Image_Width=2048, Im
 		dimension of the image (default to 2048x2048 if no binning)
 	
 	Returns
-	------
+	-------
 	Xout, Yout: float
 		X,Y Coordinates of the item in IM standards
-	'''
+	"""
 	# Do the conversion
 	Xout = X0mm + (-Image_Width/2  + Xpix)*PixelSize_um*10**-3        	     # result in mm
 	Yout = Y0mm + (-Image_Height/2 + Image_Height -Ypix)*PixelSize_um*10**-3 # Consider for Y the 0 pixel coordinates at the image bottom (ie Y axis oriented towards the top). From the image center, substract half the image towards the bottom (-Image_Height/2, we are now at the image bottom), then add the Image height (+ Image_Height, we are at the image top) then substract the Y center to go down to the y item coordinates
