ea5f7a3c5995cb249eb839f84b9a79578b032cec
Laurent Thomas <laurent132.thomas@laposte.net>
2021-01-08 16:26:38 +0100

    add test for TCPIP

---
+++ test_tcpip.py
@@ -0,0 +1,23 @@
+"""
+Test the TCPIP port of the IM
+Make sure to set the active directory to the root of the repo using cd in the command prompt
+"""
+from acquifer import IM
+
+im = IM.TCPIP()
+print(im) # should print status, version and port
+
+# Check XYZ-position
+def printPosition():
+    print("X-position: ", im.getXaxis())
+    print("Y-position: ", im.getYaxis())
+    print("Z-position: ", im.getZaxis())
+
+printPosition()
+
+# Go to XYZ-position
+im.goToXY(12.345, 23.456)
+im.goToZ(20.123)
+
+# Check after moving
+printPosition()
