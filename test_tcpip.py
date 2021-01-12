03ac9b65f0fad92df1a7a26eb4f13ee3112a8dc8
Laurent Thomas <laurent132.thomas@laposte.net>
2021-01-12 16:03:00 +0100

    Add more tests (validated)

--- test_tcpip.py
+++ test_tcpip.py
@@ -4,12 +4,14 @@ Make sure to set the active directory to the root of the repo using cd in the co
 Also make sure that the smart imaging test VI is not running simultaneously, it is also esablishing a connection and would thus prevent new ones
 """
 from acquifer import IM
+import time
 
 im = IM.TCPIP()
 print(im) # should print status, version and port
 
 #%% Check XYZ-position
 def printPosition():
+    print("") # new line
     print("X-position: ", im.getXaxis())
     print("Y-position: ", im.getYaxis())
     print("Z-position: ", im.getZaxis())
@@ -17,13 +19,30 @@ def printPosition():
 printPosition()
 
 #%% Get objective
-print("Objective number: ", im.getObjectiveNo())
+print("\nObjective number: ", im.getObjectiveNo())
+print("Channel number: ", im.getLightNo())
 
 #%% Go to XYZ-position
+print("\nMove objective along XYZ")
 im.goToXY(12.345, 23.456)
 im.goToZ(20.1)
 
 # Check after moving
 printPosition()
 
+#%% setScript, start, stop
+print("Start a known script and stop it after 20s")
+scriptPath = r"D:\IMAGING-DATA\SCRIPTS\2x.imsf"
+im.setScriptFile(scriptPath)
+im.startScript()
+time.sleep(20) # wait 20 seconds
+im.stopScript()
+
+#%% Experiment file
+print("Load preconfigured experiment (imef file), this should update the GUI accordingly")
+imef = "D:\IMAGING-DATA\EXPERIMENT\DEFAULT\Default.imef"
+im.openExperimentFile(imef)
+
+#%% Close socket at the end
+print("\nClose connection port")
 im.closeSocket()
