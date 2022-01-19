b91accf65ef67f9544b4cd9635f27425276ae7fd
Laurent Thomas <laurent132.thomas@laposte.net>
2022-01-19 14:28:25 +0100

    remove example keep test

--- Example.py
+++
@@ -1,6 +0,0 @@
-from acquifer.tcpip import IM
-
-im = IM()
-im.setMode("script")
-im.setBrightField()
-im.closeSocket()
--- test_tcpip.py
+++ test_tcpip.py
@@ -1,56 +1,25 @@
 """
 Test the TCPIP port of the IM
 Make sure to set the active directory to the root of the repo using cd in the command prompt
-Also make sure that the smart imaging test VI is not running simultaneously, it is also esablishing a connection and would thus prevent new ones
-
-Tested with v 4.10.07
 """
-from acquifer import IM
-import time
+from acquifer import tcpip
 
-im = IM.TCPIP()
-print(im) # should print status, version and port
+#%% Open tcpip communication and switch to script mode
+im = tcpip.IM()
+im.setMode("script")
+print("Connected and activated script mode.")
 
-#%% image file
-print("Current image file", im.getImageFile())
+#%% Acquire a brightfield image
+print("Set Metadata and start acquisition.")
+im.setBrightField(6, 2, 50, 100)
+im._setImageFilenameAttribute("CO", 1) # overwrite channel number define when imaging
+im.setSubposition(10)
+im.setWellNumber(10)
+im.setWellId("B012")
+im.setTimepoint(2)
+im.acquire(200, 5, 10, r"C:\Users\Administrator\Desktop\Laurent")
 
-#%% Lid
-print("Lid position: ", im.getLidAxis())
-
-#%% Check XYZ-position
-def printPositions():
-    print("") # new line
-    print("X-position: ", im.getXaxis())
-    print("Y-position: ", im.getYaxis())
-    print("Z-position: ", im.getZaxis())
-
-printPositions()
-
-#%% Get objective
-print("\nObjective number: ", im.getObjectiveNo())
-print("Channel number: ", im.getLightNo())
-
-#%% Go to XYZ-position
-print("\nMove objective along XYZ")
-im.goToXY(12.345, 23.456)
-im.goToZ(20.1)
-
-# Check after moving
-printPositions()
-
-#%% setScript, start, stop
-print("Start a known script and stop it after 20s")
-scriptPath = r"D:\IMAGING-DATA\SCRIPTS\2x.imsf"
-im.setScriptFile(scriptPath)
-im.startScript()
-time.sleep(20) # wait 20 seconds
-im.stopScript()
-
-#%% Experiment file
-print("Load preconfigured experiment (imef file), this should update the GUI accordingly")
-imef = "D:\IMAGING-DATA\EXPERIMENT\DEFAULT\Default.imef"
-im.openExperimentFile(imef)
 
 #%% Close socket at the end
-print("\nClose connection port")
+print("\nClose connection port, go back to live mode.")
 im.closeSocket()
