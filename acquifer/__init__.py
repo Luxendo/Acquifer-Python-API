c6295a0f5be87ef7cb82bf894718eda019e95b55
Laurent Thomas <laurent132.thomas@laposte.net>
2022-01-19 12:01:27 +0100

    move tcpip to acquifer module

--- acquifer/__init__.py
+++ acquifer/__init__.py
@@ -18,7 +18,7 @@ To reduce the length of this single file, the method bodies are separated in pri
 which are imported at the top of this script
 """
 from __future__ import division
-from . import _im, _im03, _im04
+from . import _im, _im03, _im04, tcpip
 import socket, string
 
 
