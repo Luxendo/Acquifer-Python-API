bf55c8389d597e3f777ff38180dc20d7cbd4b00f
Laurent Thomas <laurent132.thomas@laposte.net>
2022-01-20 17:22:51 +0100

    Update test_tcpip.py

--- test_tcpip.py
+++ test_tcpip.py
@@ -6,18 +6,16 @@ from acquifer import tcpip
 
 #%% Open tcpip communication and switch to script mode
 im = tcpip.IM()
-im.setMode("script")
-print("Connected and activated script mode.")
+
 
 #%% Acquire a brightfield image
 print("Set Metadata and start acquisition.")
-im.setBrightField(6, 2, 50, 100)
-im._setImageFilenameAttribute("CO", 1) # overwrite channel number define when imaging
-im.setSubposition(10)
+
+im.setSubposition(3)
 im.setWellNumber(10)
 im.setWellId("B012")
 im.setTimepoint(2)
-im.acquire(200, 5, 10, r"C:\Users\Administrator\Desktop\Laurent")
+im.acquire(1, "BF", 4, 50, 100, 200, 10, 5, False, r"C:\Users\Administrator\Desktop\Laurent")
 
 
 #%% Close socket at the end
