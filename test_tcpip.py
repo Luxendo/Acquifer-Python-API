f3bfc078559f239c63c5790bf6c7b28a3168cf48
Laurent Thomas <laurent132.thomas@laposte.net>
2022-01-27 14:51:11 +0100

    Update test_tcpip.py

--- test_tcpip.py
+++ test_tcpip.py
@@ -8,13 +8,19 @@ from acquifer import tcpip
 im = tcpip.IM()
 
 
-#%% Acquire a brightfield image
+#%% Acquire an image
 print("Set Metadata and start acquisition.")
 
 im.setSubposition(3)
-im.setWellNumber(10)
-im.setWellId("B012")
+#im.setBrightField(6, 2, 50, 100)
+im.setFluoChannel(1, "010000", 3, 40, 120)
+
+im._setImageFilenameAttribute("CO", 1) # overwrite channel number define when imaging
+im.setSubposition(10)
+im.setWellNumber(3)
+im.setWellId("B001")
 im.setTimepoint(2)
+
 im.acquire(1, "BF", 4, 50, 100, 200, 10, 5, False, r"C:\Users\Administrator\Desktop\Laurent")
 
 
