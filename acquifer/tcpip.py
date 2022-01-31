cf358567198159b257584a87e7d914cf858938d7
Laurent Thomas <laurent132.thomas@laposte.net>
2022-01-31 16:34:16 +0100

    wait for feedback print the exception received from tcpip

--- acquifer/tcpip.py
+++ acquifer/tcpip.py
@@ -141,8 +141,9 @@ class IM(object):
 		This function calls getFeedback with the correct size corresponding to "finished". 
 		It will pause code execution until this amount of bytes can be read.
 		"""
-		if self._getFeedback(10) != "finished": # chose 10 but could be another value
-			raise Exception("Issue executing command.")
+		feedback = self._getFeedback()
+		if feedback != "finished":
+			raise Exception(feedback)
 
 	def _getValueAsType(self, command, cast):
 		"""Send a command, get the feedback and cast it to the type provided by the cast function ex: int."""
