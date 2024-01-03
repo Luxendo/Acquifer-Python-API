11809165698e1b9b177bf4a8315ac63eac5ed927
Laurent Thomas <laurent.thomas@bruker.com>
2024-01-03 17:52:28 +0100

    remove print statement from acquifer.scripts

--- acquifer/scripts.py
+++ acquifer/scripts.py
@@ -4,22 +4,11 @@ This module holds function to handle IM scripts, for instance to replace objecti
 
 @author: Laurent Thomas - Acquifer / Luxendo GmbH
 """
-
-#from importlib_resources import files
 import os, clr, sys
 
 dllDir = os.path.join(os.path.dirname(__file__), 'dlls')
 #print(dllDir)
 
 sys.path.append(dllDir)
-
 clr.AddReference("ScriptUtils")
-
-
 from ScriptUtils import ScriptModifier, WellInfo
-
-well = WellInfo("a001", 1, 1 , 1, 1)
-print(str(well)) # priting the object wont show the string representation, so first need to convert to string
-
-#listWells = WellInfo
-#ScriptModifier.ReplacePositions
