b2fa17c02fb8c293b622ce5d72a8c401fb485f87
Laurent Thomas <laurent.thomas@bruker.com>
2024-01-17 16:49:46 +0100

    update dll with PixelPosition cosntructor with Z and image filepath as string

--- tests/Replace_PixelPositions_Script.py
+++ tests/Replace_PixelPositions_Script.py
@@ -1,5 +1,5 @@
 """
-This script will replace the objective positions in an existing IM script with custom user-provided positions.  
+This script will replace the objective positions in an existing IM script with custom user-provided positions.	
 Similar to what the PlateViewer is doing with pre/screen.
 This script is motsly for testing, the version that is replacing with pixel positions in images is more interesting for image-analysis.
 
@@ -12,22 +12,24 @@ REQUIREMENTS :
 	the control software of the IM opened
 """
 
-#%% Import and open tcpip communication
+#%% Import
 from acquifer import tcpip, scripts
 from ScriptUtils import PixelPosition
 
-path_rescreen_script = "C:\Users\Laurent\Downloads\10x-bf.imsf" # this script is only used to get the high-res settings, positios will be replaced with the one defined below
+#%% Replace position in rescreen script
+path_rescreen_script = r"C:\Users\Laurent\Downloads\10x-bf.imsf" # this script is only used to get the high-res settings, positios will be replaced with the one defined below
 
-# Replace with PixelPosition
-image_filename = "D:\IMAGING-DATA\IMAGES\20240117_152537_default\-A001--PO01--LO001--CO1--SL001--PX32500--PW0040--IN0030--TM202--X014835--Y010309--Z192001--T0000000000--WE00001.tif"
+image_filename1 = r"D:\IMAGING-DATA\IMAGES\20240117_152537_default\-A001--PO01--LO001--CO1--SL001--PX32500--PW0040--IN0030--TM202--X014835--Y010309--Z192001--T0000000000--WE00001.tif"
+A3 = r"D:\IMAGING-DATA\IMAGES\20240117_152537_default\-A003--PO01--LO001--CO1--SL001--PX32500--PW0040--IN0030--TM202--X032843--Y010309--Z192001--T0000004621--WE00003.tif"
 
 zref = 19449.7 # in um, this will be the Z-position for imaging, or for the starting point of the AF
 
-listPositions = [PixelPosition(424, 1696, zref, image_filename, 1),
-                 PixelPosition(240, 1000, zref, image_filename, 2)]
+listPositions = [PixelPosition(424, 1696, zref, image_filename1, 1),
+				 PixelPosition(240, 1000, zref, image_filename1, 2),
+				 PixelPosition(1232, 504, zref, A3, 1)]
 
 
-script = scripts.ReplacePositionsInScriptFile(path, listPositions)
+script = scripts.replacePositionsInScriptFile(path_rescreen_script, listPositions)
 
 
 #%% Run script with new positions
